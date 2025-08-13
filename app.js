50ce3d3ad2391391513c82d3d0c6b5326327edb8
Edwin Llanos <alejo.lla.99@gmail.com>
2025-08-12 21:25:20 -0500

    declarando una variable tipo array

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Inicia declarando una variable de tipo array, que almacenará los nombres de los amigos ingresados. 
+let lista_de_nombres =  amigos[0];
+
+console.log(lista_de_nombres);

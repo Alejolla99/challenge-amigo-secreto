908673daf3512256c12b8df89ac1212330a40ffd
Edwin Llanos <alejo.lla.99@gmail.com>
2025-08-15 01:31:12 -0500

    se crea una lista visible

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ function agregarAmigo() {
         
         registrodeamigos.push(lista_de_amigos);
         document.getElementById(`amigo`).value = "";
+        
+        lista_del_sorteo();
 
     } else{
         alert (`Error: Por favor, inserte un nombre`);
@@ -17,7 +19,15 @@ function agregarAmigo() {
 
     return; 
 }
-
+function lista_del_sorteo (){
+    const lista = document.getElementById("lista_del_sorteo");
+    lista.innerHTML = "";
+        for (let i = 0; i < registrodeamigos.length; i++){
+            const nueva_lista = document.createElement("li");
+            nueva_lista.textContent = registrodeamigos[i];
+            lista.appendChild(nueva_lista);
+        }
+}
 
 
 
